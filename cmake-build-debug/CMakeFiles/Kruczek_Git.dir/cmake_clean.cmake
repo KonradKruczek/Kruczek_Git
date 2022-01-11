9e7d45ae8815b1312ed25eb3f9cfefc419ce0c76
KonradKruczek <240720@edu.p.lodz.pl>
2022-01-11 23:19:32 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Kruczek_Git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kruczek_Git.dir/main.cpp.obj"
+  "Kruczek_Git.exe"
+  "Kruczek_Git.exe.manifest"
+  "Kruczek_Git.pdb"
+  "libKruczek_Git.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kruczek_Git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Kruczek_Git.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Kruczek_Git.
+# This may be replaced when dependencies are built.
